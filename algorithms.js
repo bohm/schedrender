79cf39dcf536a67fafbd1fc7aeee03f98d3c9feb
Martin Böhm <bohm+git@atrey.karlin.mff.cuni.cz>
2019-09-25 00:33:22 +0200

    Algorithm progress.

--- algorithms.js
+++ algorithms.js
@@ -1,18 +1,80 @@
 // Algorithms that use scheduling definitions.
 // import Heap from "https://unpkg.com/heap-js@1.4.1/dist/heap-js.es5.js";
 
+// A simple time event with an explanation
+class Event
+{
+    time;
+    jobID;
+    type;
+    constructor(time, jobID, type)
+    {
+	this.time = time;
+	this.type = type;
+    }
+};
+
+Event.Released = 0;
+Event.Pacing = 1;
+Event.Finished = 2;
+
+function eventComparator(a, b)
+{
+    return a.time - b.time;
+}
+
+// TODO: a multiarray class that essentially hosts an array of sets
+// and allows for iteration.
+
+class Multiarray
+{
+    cap;
+    ar = [];
+
+    constructor(cap)
+    {
+	this.cap = cap;
+	for (let i = 0; i < cap; i++)
+	{
+	    this.ar.push(new Set());
+	}
+    }
+
+    add(intKey, value)
+    {
+	this.ar[intKey].add(value);
+    }
+
+    delete(intKey, value)
+    {
+	this.ar[intKey].delete(value);
+    }
+
+    contains(intKey, value)
+    {
+	if (intKey < 0 || intKey >= this.cap)
+	{
+	    return false;
+	}
+	
+	return ar[intKey].has(value);
+    }
+    
+};
+
 function Yardstick(instance)
 {
     // Ignore parallelization and speeds, yardstick only works with
     // speed 1.0 and full paralelization.
-    const minHeap = new Heap();
-    const maxHeap = new Heap(Heap.maxComparator);
- 
-    minHeap.init([5, 18, 1]);
-    minHeap.push(2);
-    console.log(minHeap.peek()); //> 1
-    console.log(minHeap.pop()); //> 1
-    console.log(minHeap.peek()); //> 2
+
+    // Array of currently running jobs.
+    var runningJobs = [];
+
+    // Multiset of currently pending jobs.
+
+    // Heap of all time events
+    const eventHeap = new Heap(eventComparator);
+    
 }
 
 Yardstick();
